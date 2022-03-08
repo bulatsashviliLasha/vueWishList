f1748dc5c7b823251dbba5e4384259cad1e4f288
Lasha <chardaxi@outlook.com>
2022-03-08 13:15:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+Vue.createApp({
+    data(){
+        return{
+            plText: "add wish",
+            title: "Gvantsa's wishes",
+            inputValue: '',
+            notes: ['Go on a safari in africa','Swim with dolphins'],
+        }
+    },
+    methods: {
+        addNewNote(){
+            if(this.inputValue !== ''){
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        }
+        ,
+        removeNote(index){
+         this.notes.splice(index,1)
+        },
+    },
+
+    computed:{
+      doubleCount(){
+          return  this.notes.length * 2
+      },
+
+    },
+    watch:{
+      inputValue(value){
+          if(value.length>20){
+              this.inputValue = ''
+          }
+
+      }
+    }
+}).mount('#app')
